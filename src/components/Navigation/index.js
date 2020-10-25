d854302843611928d32a46163e3338915bba1390
Dan Duffy <locus78@gmail.com>
2020-10-24 18:27:32 -0700

    login and logout working

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignOutButton from '../SignOut';
+
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
   <div>
     <ul>
       <li>
         <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
       </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
@@ -23,7 +26,20 @@ const Navigation = () => (
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
       </li>
+      <li>
+        <SignOutButton />
+      </li>
     </ul>
   </div>
+);
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
 );
 export default Navigation;
